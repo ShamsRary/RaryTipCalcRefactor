c161f034d71b1aa77e50f547f0637008391b8be5
Shamsrary@gmail.com <Shamsrary@gmail.com>
2022-10-07 12:31:23 -0400

    refractor

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class TipCalculator {
+public class TipCalculator {
+    private int numPeople;
+    private int tipPercentage;
+    private double totalBillBeforeTip;
+
+    public TipCalculator(int numPeople, int tipPercentage)
+    {
+        this.numPeople = numPeople;
+        this.tipPercentage = tipPercentage;
+        totalBillBeforeTip = 0.0;
+    }
+
+    double getTotalBillBeforeTip()
+    {
+        return totalBillBeforeTip;
+    }
+
+    double getTipPercentage()
+    {
+        return tipPercentage;
+    }
+
+    double addMeal(double cost)
+    {
+        totalBillBeforeTip += cost;
+        return totalBillBeforeTip;
+    }
+
+    double tipAmount()
+    {
+        double totalTipAmount = tipPercentage * totalBillBeforeTip * 0.01;
+        return totalTipAmount;
+    }
+
+    double totalBill()
+    {
+        //double totalTipAmount = 0;
+        double totalCost = tipAmount() + totalBillBeforeTip;
+        return totalCost;
+    }
+
+    double perPersonCostBeforeTip()
+    {
+        double totalCosBeforetPerPerson = getTotalBillBeforeTip()/numPeople;
+        return totalCosBeforetPerPerson;
+    }
+
+    double perPersonTipAmount()
+    {
+        double totalTipAmountPerPerson = tipAmount() / numPeople;
+        return totalTipAmountPerPerson;
+    }
+
+    double perPersonTotalCost()
+    {
+        double totalPerPerson = perPersonTipAmount() + perPersonCostBeforeTip();
+        return totalPerPerson;
+    }
+
+
+
+
 }
+
--- src/TipCalculatorRunner.java
+++ src/TipCalculatorRunner.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class TipCalculatorRunner {
+import java.util.Scanner;
+public class TipCalculatorRunner {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Type in your Tip Percentage.");
+        int tipPercentage = input.nextInt();
+
+        System.out.println("Type in your the amount of people in your party.");
+        int totalpeople = input.nextInt();
+
+        TipCalculator wow = new TipCalculator(totalpeople, tipPercentage);
+
+        System.out.println("Welocome user type in your bill.");
+        double totalBill = input.nextDouble();
+        wow.addMeal(totalBill);
+
+
+        double totaltip = wow.tipAmount();
+        double totalCost1 = wow.totalBill();
+        double billPerPerson = wow.perPersonCostBeforeTip();
+        double tipPerPerson =  wow.perPersonTipAmount();
+        double totalPerPerson = wow.totalBill();
+
+        System.out.println("Total tip = " + totaltip);
+        System.out.println("Total cost including tip = " + totalCost1);
+        System.out.println("Total tip for a person = " + tipPerPerson);
+        System.out.println("Total cost not including tip for a person = " +  billPerPerson);
+        System.out.println("Total cost including tip for a person = " +  totalPerPerson);
+        System.out.println("Thank you for using");
+
+    }
 }
